c5960e489f45f2ede8359bda6e0bf31d2985dac3
biacullen <biancajesusdesouzaa@gmail.com>
2023-12-09 17:28:40 -0300

    up

--- js/mostrarMenu.js
+++ js/mostrarMenu.js
@@ -1,18 +1,29 @@
-function mostrarMenu () {
-    var menu = document.getElementById("menu-header");
-    var icone = document.getElementById("icone-menu");
+// script responsável pela automação do menu hamburger
 
-    if (getComputedStyle(menu).display == 'none') {
-        menu.style.display = "flex";
-        icone.classList.remove ("fa-bars");
-        icone.classList.add ("fa-times");
-        // Mudando o class para fa-times
-    } else {
-        menu.style.display = "none";
-        icone.classList.remove ("fa-times");
-        icone.classList.add ("fa-bars");
-        // Mudando o class para fa-bars [ icone barras ]
-    } 
+const btnMobile = document.getElementById('btn-mobile');
+// const >>declara a nossa variável, no uso do decorrer do código
+
+function toggleMenu(event) {    
+    if (Event.type === 'touchstart') Event.prevenDefault ();
+    const nav = document.getElementById ('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    Event.currenTarget.setAttribute ('aria-expanded', active);
+    if (active){
+        Event.currenTarget.setAttribute('aria-label', 'fechar Menu');
+    }else{
+        Event.currenTarget.setAttribute*('aria-label','abir Menu');
+        }    
+
 
 
 }
+
+    
+
+    btnMobile.addEventListener('click',toggleMenu)
+    btnMobile;addEventListener('touchstart',toggleMenu)
+    
+
+
+  
