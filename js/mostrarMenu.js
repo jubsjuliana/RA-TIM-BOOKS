3a4c0d2a4e36cdd3a7bb8a33e3f650bff931d674
kethellynfreitas <kethellynfreitas>
2023-12-09 17:22:39 -0300

    update ratim333

--- js/mostrarMenu.js
+++ js/mostrarMenu.js
@@ -1,18 +1,20 @@
-function mostrarMenu () {
-    var menu = document.getElementById("menu-header");
-    var icone = document.getElementById("icone-menu");
+// Script responsável pela automoção do menu hamburger
 
-    if (getComputedStyle(menu).display == 'none') {
-        menu.style.display = "flex";
-        icone.classList.remove ("fa-bars");
-        icone.classList.add ("fa-times");
-        // Mudando o class para fa-times
-    } else {
-        menu.style.display = "none";
-        icone.classList.remove ("fa-times");
-        icone.classList.add ("fa-bars");
-        // Mudando o class para fa-bars [ icone barras ]
-    } 
+const btnMobile = document.getElementById('btn-mobile');
+// const >> declara a nossa variável, no uso decorrer do códico 
 
-
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDeFault();
+    const nav = document.getElementById('nav');
+      nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+      } else {
+        event.currentTarget.setAttribute('arial-label', 'Abrir Menu');
+    }
 }
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
